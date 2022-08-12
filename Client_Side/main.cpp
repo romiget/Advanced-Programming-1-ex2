709ba52eb0adf6684b40a610e093238790d1a07d
ido <eadogold@gmail.com>
2022-08-12 15:31:11 +0300

    fixed a minor bug in main

--- Client_Side/main.cpp
+++ Client_Side/main.cpp
@@ -8,9 +8,10 @@ int main(int argc, char* argv[]) {
     Communication::init();
     Communication::connectToServer();
     fstream fs;
+    fs.open(argv[1]);
     string line;
     while (getline(fs, line)) {
-        Communication::sendData(argv[1]);
+        Communication::sendData(&line.front());
         Communication::recvData(argv[2]);
     }
 }
